4e94a14b522b2a24ad4dc84bedda94ae2d008f99
Mathieu GOULIN <mathieu.goulin@gadz.org>
2012-12-30 18:53:00 +0100

    Add test for LdapFilter class

--- Exception/Filter/InvalidLdapFilterException.php
+++ Exception/Filter/InvalidLdapFilterException.php
@@ -18,45 +18,7 @@
  * Foundation, Inc.,                                                       *
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA                   *
  ***************************************************************************/
- 
-namespace Gorg\Bundle\LdapOrmBundle\Ldap\Filter;
 
-use Gorg\Bundle\LdapOrmBundle\Exception\Filter\InvalidLdapFilterException;
+namespace Gorg\Bundle\LdapOrmBundle\Exception\Filter;
 
-class LdapFilter
-{
-	private $filterData;
-        private $operator;
-
-	function __construct($filterArray = array(), $operator = "AND")
-	{
-		$this->filterData = $filterArray;
-                if($operator == "AND") {
-                    $this->operator = "&";
-                } elseif($operator == "OR") {
-                    $this->operator = "|";
-                } else {
-                    throw new InvalidLdapFilterException(sprintf('The second argument of LdapFilter must be "OR" or "AND" ("%s" given)', $operator));
-                }
-	}
-
-	public function format($type = null)
-	{
-		$returnString = "";
-                $sufix = "";
-                if(count($this->filterData) > 1) 
-                {
-                    $returnString .= '(' . $this->operator;
-                    $sufix .= ')';
-                }
-		foreach($this->filterData as $key => $value)
-		{
-			$returnString .= '('  . $key . '=' . $value . ')';
-		}
-                $returnString .= $sufix;
-                if($returnString == null) {
-                    $returnString="objectclass=*";
-                }
-		return $returnString;
-	}
-}
+class InvalidLdapFilterException extends \Exception { } 
